124ff6313f8b1544a4ea813721997a283a9e4f93
kakashi-hatake3 <mayorov.art7@gmail.com>
2020-02-22 16:46:52 +1000

    first commit

---
+++ MapAPI.py
@@ -0,0 +1,37 @@
+import requests
+
+
+class MapAPI():
+    def __init__(self):
+        self.cords = ['135.232', '45.214']
+        self.zoom = '15'
+        self.mod = 0  # вид карты
+        self.api_server = "http://static-maps.yandex.ru/1.x/"
+
+    def draw(self):  # обновляет файл с картой
+        if self.mod % 3 == 0:
+            map_kind = 'sat'
+        elif self.mod % 3 == 1:
+            map_kind = 'map'
+        elif self.mod % 3 == 2:
+            map_kind = 'skl'
+        params = {
+            "ll": ",".join([self.cords[0], self.cords[1]]),
+            "z": self.zoom,
+            "l": map_kind
+        }
+        response = requests.get(self.api_server, params=params)
+        # Запишем полученное изображение в файл.
+        map_file = "map.png"
+        with open(map_file, "wb") as file:
+            file.write(response.content)
+
+    def find(self, find_object):
+        geocoder_request = "http://geocode-maps.yandex.ru/1.x/?apikey=40d1649f-0493-4b70-98ba-98533de7710b&" \
+                   "geocode=" + find_object + "&format=json"
+        response = requests.get(geocoder_request)
+        if response:
+            json_response = response.json()
+            toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
+            self.cords = toponym["Point"]["pos"].split()
+            self.draw()
